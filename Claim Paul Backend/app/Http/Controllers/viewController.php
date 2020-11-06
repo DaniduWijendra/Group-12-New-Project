1df89c85bd0af06d3946ec2ab479c678f9080019
Danidu <daniduwijendra@gmail.com>
2020-11-06 14:51:36 +0530

    commit from buddhi

--- Claim Paul Backend/app/Http/Controllers/viewController.php
+++ Claim Paul Backend/app/Http/Controllers/viewController.php
@@ -14,7 +14,7 @@ class viewController extends Controller
          ->join('reports','reports.vehicleNumber','vehicles.vehicleNumber')
          ->select('policy_holders.pId','policy_holders.NIC','policy_holders.fName','policy_holders.lName',
          'policy_holders.pAddress','policy_holders.pDOB','policy_holders.pContactNo',
-         'vehicles.vehicleNumber','reports.rDescription','reports.rCost','reports.rDate',
+         'vehicles.vehicleNumber','reports.rId','reports.rDescription','reports.rCost','reports.rDate',
          'reports.place','reports.adminId','reports.agId','reports.isAccepted')->get();
     }
 }
